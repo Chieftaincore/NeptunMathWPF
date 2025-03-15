fd8d623e726fcf260410fea8198b45bce9697dfd
Huseyin cht <Bilgisayar@DESKTOP-MODLPQO>
2025-03-16 02:52:20 +0300

    Proje dosyası ekle.

---
+++ NeptunMathWPF/LatexTestPencere.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NeptunMathWPF
+{
+    /// <summary>
+    /// LatexTestPencere.xaml etkileşim mantığı
+    /// </summary>
+    public partial class LatexTestPencere : Window
+    {
+        public LatexTestPencere()
+        {
+            InitializeComponent();
+        }
+
+        private void tusCevir_Click(object sender, RoutedEventArgs e)
+        {
+            //Try Catch düzenlenecek
+            try
+            {
+                string LatexYazi = LatexGirdi.Text;
+                LatexCikti.Formula = LatexYazi;
+            }
+            catch
+            {
+                MessageBox.Show("Hata", "Latex Hatası");
+            }
+        }
+    }
+}
---
+++ NeptunMathWPF/TestPencereWPF.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NeptunMathWPF
+{
+    /// <summary>
+    /// TestPencereWPF.xaml etkileşim mantığı
+    /// </summary>
+    public partial class TestPencereWPF : Window
+    {
+        public TestPencereWPF()
+        {
+            InitializeComponent();
+        }
+
+        //HES kod
+        //Yeni Latex pencere açar 
+        private void LaTeXtestformuTikla(object sender, RoutedEventArgs e)
+        {
+            (new LatexTestPencere()).Show();
+            //WPF'de Close() kaynakları siler Hide() ise gizler
+            this.Close();
+        }
+    }
+}
