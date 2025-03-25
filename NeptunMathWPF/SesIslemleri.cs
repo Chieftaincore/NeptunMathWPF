1560a54685f64042cf73fff73ab891ce817402da
Enes <enessaban16@gmail.com>
2025-03-25 19:25:28 +0300

    Ses oynatma fonksiyonu ekledim kodun içinde kullanmak için

---
+++ NeptunMathWPF/SesIslemleri.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+//Enes
+//Genel ses islemleri için oluşturduğum class buradan metodları çağırıp kullanıcaz mesela soru yanlışsa if komuduyla yanlış
+//sesi oynatıcaz bunu da sesler klasörüne ses ekleriz yanlış sesini koyup oradan dosya yoluyla çağırırız SesIslemleri.sesOynat()
+//ses dosyaları .wav türünde olmalı dosya formatından değiştiririz
+//örnek dosya yolu  C:/Users/Sounds/yanlisSesi.wav
+public static class SesIslemleri
+{
+    public static void sesOynat(string DosyaYolu)
+    {
+        SoundPlayer player = new SoundPlayer(@"" + DosyaYolu);
+        player.Play();
+        
+    }
+}
