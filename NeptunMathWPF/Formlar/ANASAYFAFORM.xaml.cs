7f9d0ee3530f3c0f623b010a4fa5731fdfe75a5e
Emir Öncel <emironcel16@hotmail.com>
2025-04-07 17:45:40 +0300

    login ve anamenü geldi deneme pushuMerge branch 'master' into Ses_sistemi

---
+++ NeptunMathWPF/Formlar/ANASAYFAFORM.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+
+namespace AnaSayfaForm
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SoruButton_Click(object sender, RoutedEventArgs e)
+        {
+            // SoruFormu penceresini aç
+            SoruFormu soruFormu = new SoruFormu();
+            soruFormu.Show();
+            MessageBox.Show("Sorulara sayfasına gidiliyor");
+        }
+
+        private void SoruUreticiButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Soru üretici sayfasına yönlendirme kodları buraya gelecek
+            SoruSec soruSec = new SoruSec();
+            soruSec.Show();
+            MessageBox.Show("Soru üretici sayfasına gidiliyor");
+        }
+    }
+}
---
+++ NeptunMathWPF/Formlar/SoruForm.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+
+namespace AnaSayfaForm
+{
+    public partial class SoruSec : Window
+    {
+        public SoruSec()
+        {
+            InitializeComponent();
+        }
+
+        private void MatematikSorulariButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Matematik soruları sayfasına yönlendirme kodları buraya gelecek
+            MessageBox.Show("Matematik soruları sayfasına gidiliyor...");
+        }
+
+        private void TarihSorulariButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Tarih soruları sayfasına yönlendirme kodları buraya gelecek
+            MessageBox.Show("Tarih soruları sayfasına gidiliyor...");
+        }
+
+        private void CografyaSorulariButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Coğrafya soruları sayfasına yönlendirme kodları buraya gelecek
+            MessageBox.Show("Coğrafya soruları sayfasına gidiliyor...");
+        }
+    }
+}
