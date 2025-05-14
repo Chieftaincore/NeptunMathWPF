837a5bb3001a1432591921a8e3f3217073902dfc
Efe Uçar <iefeucar@hotmail.com>
2025-05-14 06:05:32 +0300

    İstatistik panel bitti.

--- NeptunMathWPF/Formlar/IstatistikWPF.xaml.cs
+++ NeptunMathWPF/Formlar/IstatistikWPF.xaml.cs
@@ -32,11 +32,11 @@ namespace NeptunMathWPF.Formlar
             {
                 try
                 {
-                    if (SubtopicsListBox.SelectedItems.Count>0)
+                    if (SubtopicsListBox.SelectedItems.Count > 0)
                     {
-                    string subtopic = SubtopicsListBox.SelectedItem.ToString();
-                    int subtopicid = Genel.dbEntities.SUBTOPICS.Where(x => x.SUBTOPIC == subtopic).Select(x => x.SUBTOPIC_ID).FirstOrDefault();
-                    lblYanlisCount.Content = Genel.dbEntities.WRONG_ANSWERED_QUESTIONS.Where(x => x.SUBTOPIC_ID == subtopicid).Count();
+                        string subtopic = SubtopicsListBox.SelectedItem.ToString();
+                        int subtopicid = Genel.dbEntities.SUBTOPICS.Where(x => x.SUBTOPIC == subtopic).Select(x => x.SUBTOPIC_ID).FirstOrDefault();
+                        lblYanlisCount.Content = Genel.dbEntities.WRONG_ANSWERED_QUESTIONS.Where(x => x.SUBTOPIC_ID == subtopicid).Count();
                     }
                 }
                 catch (System.NullReferenceException)
@@ -63,6 +63,7 @@ namespace NeptunMathWPF.Formlar
             {
                 int topicid = Genel.dbEntities.TOPICS.Where(x => x.TOPIC == TopicsListBox.SelectedItem).Select(x => x.TOPIC_ID).FirstOrDefault();
                 SubtopicsListBox.ItemsSource = Genel.dbEntities.SUBTOPICS.Where(x => x.TOPIC_ID == topicid).Select(x => x.SUBTOPIC).ToList();
+                QuestionsListBox.ItemsSource = null;
             }
             catch (System.NullReferenceException) { }
         }
@@ -72,8 +73,69 @@ namespace NeptunMathWPF.Formlar
             try
             {
                 GetStatistic();
+                Genel.Handle(() =>
+                {
+                    string topicstr = TopicsListBox.SelectedItem.ToString();
+                    var topicId = Genel.dbEntities.TOPICS.Where(x => x.TOPIC == topicstr).Select(x => x.TOPIC_ID).FirstOrDefault();
+                    var subtopicId = Genel.dbEntities.SUBTOPICS.Where(x => x.TOPIC_ID == topicId).Select(x => x.SUBTOPIC_ID).FirstOrDefault();
+
+                    QuestionsListBox.ItemsSource = Genel.dbEntities.WRONG_ANSWERED_QUESTIONS.Where(x => x.SUBTOPIC_ID == subtopicId).Select(x => x.QUESTION_TEXT).ToList();
+                });
             }
             catch (System.NullReferenceException) { }
+        }
+
+        private void QuestionsListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (QuestionsListBox.SelectedItem!=null)
+            {
+
+                Genel.Handle(() =>
+                {
+                    lblA.Content = "A)";
+                    lblA.Foreground = new SolidColorBrush(Colors.Black);
+                    lblB.Content = "B)";
+                    lblB.Foreground = new SolidColorBrush(Colors.Black);
+                    lblC.Content = "C)";
+                    lblC.Foreground = new SolidColorBrush(Colors.Black);
+                    lblD.Content = "D)";
+                    lblD.Foreground = new SolidColorBrush(Colors.Black);
+                    lblE.Content = "E)";
+                    lblE.Foreground = new SolidColorBrush(Colors.Black);
+
+                    string sorustr = QuestionsListBox.SelectedItem.ToString();
+                    var soru = Genel.dbEntities.WRONG_ANSWERED_QUESTIONS.Where(x => x.QUESTION_TEXT == sorustr).FirstOrDefault();
+                    lblSoru.Content = soru.QUESTION_TEXT;
+                    List<string> answers = new List<string>();
+                    foreach (var item in soru.WRONG_ANSWERS.Split('#'))
+                    {
+                        if (!string.IsNullOrEmpty(item))
+                            answers.Add(item);
+                    }
+                    answers.Add(soru.ANSWER);
+                    Genel.Shuffle(answers);
+
+
+                    CheckTrueOrWrong(answers[0], lblA, soru.ANSWER, soru.USERS_ANSWER);
+                    CheckTrueOrWrong(answers[1], lblB, soru.ANSWER, soru.USERS_ANSWER);
+                    CheckTrueOrWrong(answers[2], lblC, soru.ANSWER, soru.USERS_ANSWER);
+                    CheckTrueOrWrong(answers[3], lblD, soru.ANSWER, soru.USERS_ANSWER);
+
+                    if (soru.WRONG_ANSWERS.Split('#')[4] != null)
+                    {
+                        CheckTrueOrWrong(answers[4], lblE, soru.ANSWER, soru.USERS_ANSWER);
+                    }
+                });
+            }
+        }
+
+        private void CheckTrueOrWrong(string answer, Label lbl, string correctanswer, string usersanswer)
+        {
+            lbl.Content += answer;
+            if (answer == correctanswer)
+                lbl.Foreground = new SolidColorBrush(Colors.Green);
+            else if (answer == usersanswer)
+                lbl.Foreground = new SolidColorBrush(Colors.Red);
         }
     }
 }
